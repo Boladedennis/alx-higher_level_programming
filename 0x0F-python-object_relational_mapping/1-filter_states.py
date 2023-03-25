74ad87eb6f7f8c2704bd1f8f76aeff05c9495c96
Bolade Dennis <you@example.com>
2023-03-25 10:49:44 -0700

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
-'''script for task 1'''
-
-import MySQLdb
+"""List all states with a name starting with uppercase N
+Username, password, and database names are given as user args
+"""
 import sys
-
-
-def list_N():
-    '''lists all states with a name that starts with N'''
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    host = 'localhost'
-    port = 3306
-
-    db = MySQLdb.connect(host=host, user=username, passwd=password,
-                         db=db_name, port=port)
-    cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name regexp "^N.*" ' +
-                'ORDER BY states.id ASC')
-    result = cur.fetchall()
-    cur.close()
-    db.close()
-    if result:
-        for row in result:
-            if row[1][0] == "N":
-                print(row)
-
+import MySQLdb
 
 if __name__ == "__main__":
-    list_N()
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+    cur = db.cursor()
+    cmd = """SELECT id, name
+             FROM states
+             WHERE name LIKE BINARY 'N%'
+             ORDER BY id ASC;"""
+    cur.execute(cmd)
+    nStates = cur.fetchall()
+
+    for state in nStates:
+        print(state)
+
+    cur.close()
+    db.close()
