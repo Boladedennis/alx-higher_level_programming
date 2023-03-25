3a31a35cc20a06acf9e1b747145b85f140f4c113
Bolade Dennis <you@example.com>
2023-03-25 10:45:32 -0700

    syspy

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-'''script lists all states from the db'''
-
-import MySQLdb
+"""List all states from a given db sorted in ascending order by id
+Username, password, and database names are given as user args
+"""
 import sys
+import MySQLdb
 
-
-def list_all():
-    '''list all states in db'''
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    host = 'localhost'
-    port = 3306
-    db = MySQLdb.connect(host=host, user=username, passwd=password,
-                         db=db_name, port=port)
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
     cur = db.cursor()
-    cur.execute('SELECT * FROM states ORDER BY id ASC;')
-    result = cur.fetchall()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+    allStates = cur.fetchall()
+
+    for state in allStates:
+        print(state)
+
     cur.close()
     db.close()
-    if result:
-        for row in result:
-            print(row)
-
-
-if __name__ == '__main__':
-    list_all()
